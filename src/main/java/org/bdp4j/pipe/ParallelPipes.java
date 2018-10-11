d05479a6b53c17279e4ceaadceb8a9f33e87111f
Yeray Lage <ylagef@gmail.com>
2018-10-11 09:55:09 +0200

    Parallel Pipes implemented. First, without threads.

--- src/main/java/org/bdp4j/pipe/ParallelPipes.java
+++ src/main/java/org/bdp4j/pipe/ParallelPipes.java
@@ -1,10 +1,10 @@
 package org.bdp4j.pipe;
 
-import java.util.ArrayList;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bdp4j.ia.types.Instance;
+
+import java.util.ArrayList;
 
 public class ParallelPipes extends Pipe {
     private static final Logger logger = LogManager.getLogger(ParallelPipes.class);
@@ -18,23 +18,64 @@ public class ParallelPipes extends Pipe {
     }
 
     @Override
-    public Instance pipe(Instance carrier) {
-        return null;
+    public Instance pipe(Instance original) {
+
+        if (outputType == null) {
+            logger.error("[PARALLEL PIPE] Not output Pipe declared.");
+            System.exit(0);
+        }
+
+        Instance originalCopy = new Instance(original); // Copy instance of original for saving Data state.
+
+        for (Pipe p : pipes) {
+            logger.info("PARALLEL PIPE " + p.getClass().getName());
+
+
+            try {
+                if (p.equals(pipes.get(0))) {
+                    // If first pipe, it is the output one. Then we use the original one.
+                    original = p.pipe(original);
+                } else {
+                    Instance copy = new Instance(originalCopy); // One copy for each pipe of parallel.
+
+                    if (copy.isValid()) {
+                        logger.info("INST " + copy.getName());
+                        p.pipe(copy); // Just process pipe for properties set.
+                    } else {
+                        logger.info("Skipping invalid instance " + copy.toString());
+                    }
+                }
+            } catch (Exception e) {
+                logger.fatal("Exception caught on pipe " + p.getClass().getName() + ". " + e.getMessage() + " while processing instance");
+                e.printStackTrace(System.err);
+                System.exit(0);
+            }
+
+        }
+
+        return original;
     }
 
     public void add(Pipe pipe, boolean isOutput) {
+        // Set input type and check if valid.
         if (pipes.isEmpty()) {
             inputType = pipe.getInputType();
-        } else {
-            if (pipes.get(pipes.size() - 1).getInputType() != pipe.getInputType()) {
-                logger.error("[PARALLEL PIPE ADD] BAD compatibility between Pipes.");
-                System.exit(0);
-            }
+        } else if (pipes.get(pipes.size() - 1).getInputType() != pipe.getInputType()) {
+            logger.error("[PARALLEL PIPE ADD] BAD compatibility between Pipes.");
+            System.exit(0);
         }
 
-        if (isOutput) outputType = pipe.getOutputType();
+        // Set output type and put output Pipe as first of array.
+        if (isOutput && !pipes.isEmpty()) {
+            outputType = pipe.getOutputType();
 
-        pipes.add(pipe);
+            Pipe first = pipes.get(0);
+            pipes.set(0, pipe); // Set output pipe as first.
+            pipes.add(first);
+        } else {
+            if (isOutput) outputType = pipe.getOutputType();
+            pipes.add(pipe);
+        }
     }
 
     @Override
