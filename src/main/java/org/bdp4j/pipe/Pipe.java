b8768409609c48f413d62cb1fa2e79441fdcaab2
María <mnloures@gmail.com>
2018-11-02 12:01:46 +0100

    Fixed bug detecting last instance
    Change the way to detect last instance.

--- src/main/java/org/bdp4j/pipe/Pipe.java
+++ src/main/java/org/bdp4j/pipe/Pipe.java
@@ -4,7 +4,7 @@
    and the rest of the project is publised under the Common Plublic License, 
    version 1.0, as published by http://www.opensource.org. For further information
    see, seee the file 'LICENSE' included in this distribution. */
-/* Copyright (C) 2002 Univ. of Massachusetts Amherst, Computer Science Dept.
+ /* Copyright (C) 2002 Univ. of Massachusetts Amherst, Computer Science Dept.
    This file is part of "MALLET" (MAchine Learning for LanguagE Toolkit).
    http://www.cs.umass.edu/~mccallum/mallet
    This software is provided under the terms of the Common Public License,
@@ -18,19 +18,19 @@ import java.util.Iterator;
 import org.bdp4j.ia.types.Instance;
 
 /**
- * The abstract superclass of all Pipes, which transform one data type to another.
- * Pipes are most often used for feature extraction.
+ * The abstract superclass of all Pipes, which transform one data type to
+ * another. Pipes are most often used for feature extraction.
  * <p>
- * A pipe operates on an {@link org.bdp4j.ia.types.Instance}, which is a carrier of data.
- * A pipe reads from and writes to fields in the Instance when it is requested
- * to process the instance. It is up to the pipe which fields in the Instance it
- * reads from and writes to, but usually a pipe will read its input from and write
- * its output to the "data" field of an instance.
+ * A pipe operates on an {@link org.bdp4j.ia.types.Instance}, which is a carrier
+ * of data. A pipe reads from and writes to fields in the Instance when it is
+ * requested to process the instance. It is up to the pipe which fields in the
+ * Instance it reads from and writes to, but usually a pipe will read its input
+ * from and write its output to the "data" field of an instance.
  * <p>
- * Pipes can be hierachically composed. In a typical usage, a SerialPipe is created which
- * holds instances of other pipes in an ordered list. Piping in instance through a 
- * SerialPipe means piping the instance through the child pipes
- * in sequence.
+ * Pipes can be hierachically composed. In a typical usage, a SerialPipe is
+ * created which holds instances of other pipes in an ordered list. Piping in
+ * instance through a SerialPipe means piping the instance through the child
+ * pipes in sequence.
  *
  * @author Andrew McCallum <a href="mailto:mccallum@cs.umass.edu">mccallum@cs.umass.edu</a>
  * @author Jose Ramon Mendez
@@ -38,15 +38,16 @@ import org.bdp4j.ia.types.Instance;
  * @author Yeray Lage
  */
 public abstract class Pipe {
-	/**
-	  * Marks if the next instance to pipe will be the last one to pipe
-	  */
+
+    /**
+     * Marks if the next instance to pipe will be the last one to pipe
+     */
     boolean isLast = false;
-	
-	/**
-	  * The parent pipe for this one
-	  */
-	Pipe parent;
+
+    /**
+     * The parent pipe for this one
+     */
+    Pipe parent;
 
     /**
      * Construct a pipe with no data and target dictionaries
@@ -55,12 +56,12 @@ public abstract class Pipe {
     }
 
     /**
-     * Process an Instance.  This method takes an input Instance,
-     * destructively modifies it in some way, and returns it.
-     * This is the method by which all pipes are eventually run.
+     * Process an Instance. This method takes an input Instance, destructively
+     * modifies it in some way, and returns it. This is the method by which all
+     * pipes are eventually run.
      * <p>
-     * One can create a new concrete subclass of Pipe simply by
-     * implementing this method.
+     * One can create a new concrete subclass of Pipe simply by implementing
+     * this method.
      *
      * @param carrier Instance to be processed.
      * @return Instancia procesada
@@ -69,70 +70,78 @@ public abstract class Pipe {
 
     /**
      * Pipe all instances from a Collection
+     *
      * @param carriers Collection of instances to pipe
      * @return The collection of instances after being processed
      */
     public Collection<Instance> pipeAll(Collection<Instance> carriers) {
-        Iterator<Instance> it=carriers.iterator();
-        while(it.hasNext()) {
+        Iterator<Instance> it = carriers.iterator();
+        while (it.hasNext()) {
             pipe(it.next());
-            isLast=!it.hasNext(); 
+            isLast = !it.hasNext();
         };
         return carriers;
     }
 
     /**
-		* Finds the parent Pipe
-		* @return the partent Pipe
-		*/
-	 public Pipe getParent() { 
-	     return parent; 
-	 } 
- 
+     * Finds the parent Pipe
+     *
+     * @return the parent Pipe
+     */
+    public Pipe getParent() {
+        return parent;
+    }
+
     /**
-		* Stablished the parent pipe for this one
-		* @param p The parent Pipe for this one
-		*/
-	 public void setParent(Pipe p) { 
-	     if (parent != null) 
-	         throw new IllegalStateException("Parent already set."); 
- 
-	      parent = p; 
-	 } 
- 
+     * Stablished the parent pipe for this one
+     *
+     * @param p The parent Pipe for this one
+     */
+    public void setParent(Pipe p) {
+        if (parent != null) {
+            throw new IllegalStateException("Parent already set.");
+        }
+
+        parent = p;
+    }
+
     /**
-		* Finds the parent root
-		* @return the root parent
-		*/
-	 public Pipe getParentRoot() { 
-	     if (parent == null) 
-	         return this; 
- 
-	     Pipe p = parent; 
- 
-	     while (p.parent != null) 
-	         p = p.parent; 
- 
-	     return p; 
-	 } 
-    
+     * Finds the parent root
+     *
+     * @return the root parent
+     */
+    public Pipe getParentRoot() {
+        if (parent == null) {
+            return this;
+        }
+
+        Pipe p = parent;
+
+        while (p.parent != null) {
+            p = p.parent;
+        }
+
+        return p;
+    }
+
     /**
      * Say whether the current Instance is the last being processed
+     *
      * @return true if the current Instance is the last being processed
      */
-    public boolean isLast(){
+    public boolean isLast() {
         return isLast;
     }
 
     /**
-     * Return the input type included the data attribute of a Instance
-     * @return the input type for the data attribute of the Instances processed
+     * Return the output type included the data attribute of a Instance
+     * @return the output type for the data attribute of the Instances processed
      */
     public abstract Class<?> getInputType();
 
     /**
-     * Indicates the datatype expected in the data attribute of a Instance after processing
-     * @return the datatype expected in the data attribute of a Instance after processing
+     * Say datatype expected in the data attribute of a Instance
+     * @return the datatype expected in the data attribute of a Instance
      */
     public abstract Class<?> getOutputType();
 }
--- src/main/java/org/bdp4j/pipe/SerialPipes.java
+++ src/main/java/org/bdp4j/pipe/SerialPipes.java
@@ -4,7 +4,7 @@
    and the rest of the project is publised under the Common Plublic License, 
    version 1.0, as published by http://www.opensource.org. For further information
    see, seee the file 'LICENSE' included in this distribution. */
-/* Copyright (C) 2002 Univ. of Massachusetts Amherst, Computer Science Dept.
+ /* Copyright (C) 2002 Univ. of Massachusetts Amherst, Computer Science Dept.
    This file is part of "MALLET" (MAchine Learning for LanguagE Toolkit).
    http://www.cs.umass.edu/~mccallum/mallet
    This software is provided under the terms of the Common Public License,
@@ -23,32 +23,38 @@ import org.bdp4j.ia.types.Instance;
 /**
  * Convert an instance through a sequence of pipes.
  *
- * @author Andrew McCallum <a href="mailto:mccallum@cs.umass.edu">mccallum@cs.umass.edu</a>
+ * @author Andrew McCallum
+ * <a href="mailto:mccallum@cs.umass.edu">mccallum@cs.umass.edu</a>
  * @author Yeray Lage
  * @author Maria Novo Loures
  * @author José Ramón Méndez
  */
 public class SerialPipes extends Pipe {
+
     private Class<?> inputType = null;
     private Class<?> outputType = null;
 
     /**
-		* For logging purposes
-		*/
+     * For logging purposes
+     */
     private static final Logger logger = LogManager.getLogger(SerialPipes.class);
-	 
+
     /**
      * Return the input type included the data attribute of a Instance
+     *
      * @return the input type for the data attribute of the Instances processed
-     */	 
+     */
     @Override
     public Class<?> getInputType() {
         return inputType;
     }
 
     /**
-     * Indicates the datatype expected in the data attribute of a Instance after processing
-     * @return the datatype expected in the data attribute of a Instance after processing
+     * Indicates the datatype expected in the data attribute of a Instance after
+     * processing
+     *
+     * @return the datatype expected in the data attribute of a Instance after
+     * processing
      */
     @Override
     public Class<?> getOutputType() {
@@ -61,26 +67,32 @@ public class SerialPipes extends Pipe {
     private ArrayList<Pipe> pipes;
 
     /**
-		* Build an empty SerialPipes
-		*/
+     * Build an empty SerialPipes
+     */
     public SerialPipes() {
         this.pipes = new ArrayList<Pipe>();
     }
 
     /**
-		* Build a serial pipes using an array of individual pipes
-		* @param pipes An array of pipes that will be included in the serialPipes in the same order
-		*/
+     * Build a serial pipes using an array of individual pipes
+     *
+     * @param pipes An array of pipes that will be included in the serialPipes
+     * in the same order
+     */
     public SerialPipes(Pipe[] pipes) {
         this.pipes = new ArrayList<Pipe>(pipes.length);
 
-        for (Pipe pipe : pipes) this.add(pipe);
+        for (Pipe pipe : pipes) {
+            this.add(pipe);
+        }
     }
 
     /**
-		* Build a serial pipes using an ArrayList of individual pipes
-		* @param pipeList An arrayList of pipes that will be included in the serialPipes in the same order
-		*/
+     * Build a serial pipes using an ArrayList of individual pipes
+     *
+     * @param pipeList An arrayList of pipes that will be included in the
+     * serialPipes in the same order
+     */
     public SerialPipes(ArrayList<Pipe> pipeList) {
         this.pipes = new ArrayList<Pipe>(pipeList.size());
 
@@ -90,11 +102,14 @@ public class SerialPipes extends Pipe {
     }
 
     /**
-		* Return an array of pipes with the current pipe
-		* @return a Pipe array containing the pipes that compound the serial pipes
-		*/
+     * Return an array of pipes with the current pipe
+     *
+     * @return a Pipe array containing the pipes that compound the serial pipes
+     */
     public Pipe[] getPipes() {
-        if (pipes == null) return new Pipe[0];
+        if (pipes == null) {
+            return new Pipe[0];
+        }
         Pipe[] returnValue = new Pipe[pipes.size()];
         return pipes.toArray(returnValue);
     }
@@ -104,12 +119,13 @@ public class SerialPipes extends Pipe {
      *
      * @param pipes pipes used
      */
-
     public void setPipes(Pipe[] pipes) {
         this.pipes = new ArrayList<Pipe>(pipes.length);
 
         //System.out.println ("SerialPipes init this = "+this);
-        for (Pipe pipe : pipes) this.add(pipe);
+        for (Pipe pipe : pipes) {
+            this.add(pipe);
+        }
     }
 
     /**
@@ -122,9 +138,10 @@ public class SerialPipes extends Pipe {
     }
 
     /**
-		* Add a new pipe at the end of the processing list
-		* @param pipe The new pipe to be added 
-		*/
+     * Add a new pipe at the end of the processing list
+     *
+     * @param pipe The new pipe to be added
+     */
     public void add(Pipe pipe) {
         if (!pipes.isEmpty()) {
             if (checkCompatibility(pipe)) {
@@ -153,10 +170,12 @@ public class SerialPipes extends Pipe {
     }
 
     /**
-		* Checks the compatibility of a pipe that will be added at the end of the pipelist
-		* @param pipe The pipe that will be added
-		* @return true if the pipe can be placed at the end of the pipelist
-		*/
+     * Checks the compatibility of a pipe that will be added at the end of the
+     * pipelist
+     *
+     * @param pipe The pipe that will be added
+     * @return true if the pipe can be placed at the end of the pipelist
+     */
     private boolean checkCompatibility(Pipe pipe) {
         Pipe lastPipe = pipes.get(pipes.size() - 1);
 
@@ -164,11 +183,12 @@ public class SerialPipes extends Pipe {
     }
 
     /**
-		* Pipe an instance starting in a certain position of the pipe
-		* @param carrier The instance to be processed
-		* @param startingIndex The position of the fisrt pipe 
-		* @return The instance achieved as result of processing
-		*/
+     * Pipe an instance starting in a certain position of the pipe
+     *
+     * @param carrier The instance to be processed
+     * @param startingIndex The position of the fisrt pipe
+     * @return The instance achieved as result of processing
+     */
     public Instance pipe(Instance carrier, int startingIndex) {
         carrier = getInstance(carrier, startingIndex);
 
@@ -176,11 +196,13 @@ public class SerialPipes extends Pipe {
     }
 
     /**
-		* Computes the instance transformation when executing the pipe processing from a certain execution point
-		* @param carrier The instance to be pipes
-		* @param startingIndex The execution point to begin the processing
-		* @return the instance after being processed
-		*/
+     * Computes the instance transformation when executing the pipe processing
+     * from a certain execution point
+     *
+     * @param carrier The instance to be pipes
+     * @param startingIndex The execution point to begin the processing
+     * @return the instance after being processed
+     */
     private Instance getInstance(Instance carrier, int startingIndex) {
         for (int i = startingIndex; i < pipes.size(); i++) {
 
@@ -207,13 +229,21 @@ public class SerialPipes extends Pipe {
         return carrier;
     }
 
-	 /**
-		* Pipe a collection of instances through the whole process
-		* @param carriers The instances to be processed
-		* @return the instances after processing them 
-		*/
+    /**
+     * Pipe a collection of instances through the whole process
+     *
+     * @param carriers The instances to be processed
+     * @return the instances after processing them
+     */
     @Override
     public Collection<Instance> pipeAll(Collection<Instance> carriers) {
+        int lastValidInstanceIdx=carriers.size()-1;
+        Instance[] carriersAsArray = carriers.toArray(new Instance[0]);
+
+        while (!carriersAsArray[lastValidInstanceIdx].isValid() ){
+            lastValidInstanceIdx--;
+        }
+
         for (int i = 0; i < pipes.size(); i++) {
             Pipe p = pipes.get(i);
             if (p == null) {
@@ -221,19 +251,18 @@ public class SerialPipes extends Pipe {
                 System.exit(0);
             } else {
                 try {
-                    Iterator<Instance> it=carriers.iterator();
-                    while (it.hasNext()) {
-                        Instance carrier=it.next();
-								
-                        if (carrier.isValid()) {
-                            //System.out.println("INST " + carrier.getName());
-									 isLast=!it.hasNext();
-                            carrier = p.pipe(carrier);
-                        }else{
-                             logger.info("Skipping invalid instance " + carrier.toString());
+                    
+                    isLast=false;
+                    for (int j=0;j<lastValidInstanceIdx;j++){
+                        if (carriersAsArray[j].isValid()) {
+                            p.pipe(carriersAsArray[j]);
+                        } else {
+                            logger.info("Skipping invalid instance " + carriersAsArray[j].toString());
                         }
                     }
-                 } catch (Exception e) {
+                    isLast=true;
+                    p.pipe(carriersAsArray[lastValidInstanceIdx]);
+                } catch (Exception e) {
                     logger.fatal("Exception caught on pipe " + i + " (" + p.getClass().getName() + "). " + e.getMessage() + " while processing instance");
                     e.printStackTrace(System.err);
                     System.exit(0);
@@ -244,22 +273,24 @@ public class SerialPipes extends Pipe {
     }
 
     /**
-		* Remove a pipe from the processing pipe
-		* @param index The position of pipe that will be removed
-		*/
+     * Remove a pipe from the processing pipe
+     *
+     * @param index The position of pipe that will be removed
+     */
     public void removePipe(int index) {
         try {
             pipes.remove(index);
         } catch (Exception e) {
-            logger.error("Error removing pipe. Index = " + index + ".  " +  e.getMessage());
+            logger.error("Error removing pipe. Index = " + index + ".  " + e.getMessage());
         }
     }
 
     /**
-		* Replace a pipe in the pipeList
-		* @param index The position of the pipe that will be replaced
-		* @param p The new Pipe
-		*/
+     * Replace a pipe in the pipeList
+     *
+     * @param index The position of the pipe that will be replaced
+     * @param p The new Pipe
+     */
     //added by Fuchun Jan.30, 2004
     public void replacePipe(int index, Pipe p) {
         try {
@@ -270,18 +301,20 @@ public class SerialPipes extends Pipe {
     }
 
     /**
-		* Computes the number of pipes included in the SerialPipes
-		* @return the number of pipes includen in current SerialPipes
-		*/
+     * Computes the number of pipes included in the SerialPipes
+     *
+     * @return the number of pipes includen in current SerialPipes
+     */
     public int size() {
         return pipes.size();
     }
 
     /**
-		* Returns the pipe included in a position
-		* @param index The position of the pipe
-		* @return the pipe included in the desired position
-		*/
+     * Returns the pipe included in a position
+     *
+     * @param index The position of the pipe
+     * @return the pipe included in the desired position
+     */
     public Pipe getPipe(int index) {
         Pipe retPipe = null;
 
@@ -295,22 +328,26 @@ public class SerialPipes extends Pipe {
     }
 
     /**
-		* Pipe a instance through the whole piping process
-		* @param carrier the instance to be piped
-		* @return The instance achieved as processing result
-		*/
+     * Pipe a instance through the whole piping process
+     *
+     * @param carrier the instance to be piped
+     * @return The instance achieved as processing result
+     */
     public Instance pipe(Instance carrier) {
         return pipe(carrier, 0);
     }
 
-	 /**
-		* Achieves a string representation of the piping process
-		* @return the string representation of the piping process
-		*/
+    /**
+     * Achieves a string representation of the piping process
+     *
+     * @return the string representation of the piping process
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        for (Pipe pipe : pipes) sb.append(pipe.toString()).append(",");
+        for (Pipe pipe : pipes) {
+            sb.append(pipe.toString()).append(",");
+        }
 
         return sb.toString();
     }
