7dde3d58b9f32d8a161f962576f2e10a3f78c2fc
Reyes Pavón <pavon@uvigo.es>
2019-05-02 10:11:47 +0200

    new version DataTimeIdentifier

--- src/main/java/org/bdp4j/util/DateTimeIdentifier.java
+++ src/main/java/org/bdp4j/util/DateTimeIdentifier.java
@@ -43,62 +43,73 @@ public class DateTimeIdentifier {
      * Singleton pattern (local reference)
      */
     private static DateTimeIdentifier defaultDateTimeProcessor = null;
-    //boolean debug = true;
-
+    
     /**
-     * Date format vector
+     * DateTime and Date format vector
      */
-    private ArrayList<DateTimeFormatter> sdfs = new ArrayList<>(); 
-    private ArrayList<DateTimeFormatter> patternsDate = new ArrayList<>();
+    private static final ArrayList<DateTimeFormatter> sdfs = new ArrayList<>();
+    private static final ArrayList<DateTimeFormatter> patternsDate = new ArrayList<>();
 
     FormatStyle[] styles = new FormatStyle[] {FormatStyle.SHORT, FormatStyle.MEDIUM, FormatStyle.LONG, FormatStyle.FULL};
     Locale[] locales = Locale.getAvailableLocales();
 
+    static{
+        
+        for (String pattern: new String[]{
+            "yyyy-MM-dd'T'HH:mm:ss", 
+            "yyyy-MM-dd'T'HH:mm", 
+            "EEE MMM dd HH:mm:ss ZZZZ yyyy", 
+            "EEE MMM dd HH:mm:ss ZZZ yyyy",
+            "EEE, dd MMM yyyy HH:mm:ss ZZZZZ (zzz)", 
+            "EEE, dd MMM yyyy HH:mm:ss ZZZZZ",
+            "dd MMM yyyy HH:mm:ss ZZZZZ",
+            "EEE, MMM dd HH:mm:ss yyyy", 
+            "MMM, dd yyyy H:mm:ss a ZZZZZ", 
+            "EEE, dd MMM yyyy HH:mm:ss +ZZZZZ", 
+            "EEE, dd MMM yyyy HH:mm:ss zz", 
+            "EEE, MMM dd H:mm:ss zzz yyyy", 
+            "EEE, d MMM HH:mm:ss yyyy ZZZZZ", 
+            "EEE, dd MMM yy HH:mm ZZZZZ", 
+            "EEE, dd MMM yyyy HH:mm:ss 'UT'", 
+            "EEE, d MMM yyyy HH:mm:ss 'UT'", 
+            "d MMM yyyy HH:mm:ss 'UT'", 
+            "dd MMM yyyy HH:mm:ss 'UT'", 
+            "EEE, dd MMM yyyy HH:mm:ss", 
+            "EEE, d MMM yyyy HH:mm:ss", 
+            "dd-MMM-yyyy HH:mm:ss ZZZZZ", 
+            "dd/MM/yyyy HH:mm:ss", 
+            "EEE, MMM dd yyyy HH:mm:ss ZZZZZ", 
+            "dd MMM yyyy HH:mm:ss", 
+            "EEE, dd, MMM yyyy HH:mm:ss ZZZZZ", 
+            "EEE, d, MMM yyyy HH:mm:ss ZZZZZ", 
+            "EEE MMM dd HH:mm:ss zzz yyyy", 
+            "MMM, dd yyyy HH:mm:ss ZZZZZ", 
+            "MMM, d yyyy HH:mm:ss ZZZZZ", 
+            "MMM, dd yyyy HH:mm:ss a", 
+            "MMM, d yyyy HH:mm:ss a"}){
+            
+            sdfs.add(DateTimeFormatter.ofPattern(pattern));
+        }
+      
+        for (String pattern: new String[]{
+            "EEE, dd MMM yyyy", 
+            "EEE, d MMM yyyy", 
+            "EEE, dd MMM yyyy zzz"}){
+            
+            patternsDate.add(DateTimeFormatter.ofPattern(pattern));
+        }   
+    }
     
     /**
      * Private constructor
      */
     private DateTimeIdentifier() {
-        sdfs.add(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
-        sdfs.add(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss ZZZZ yyyy"));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss ZZZ yyyy"));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss ZZZZZ (zzz)"));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss ZZZZZ"));
-        sdfs.add(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss ZZZZZ"));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, MMM dd HH:mm:ss yyyy").withLocale(Locale.UK));
-        sdfs.add(DateTimeFormatter.ofPattern("MMM, dd yyyy H:mm:ss a ZZZZZ"));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss +ZZZZZ"));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zz").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, MMM dd H:mm:ss zzz yyyy").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, d MMM HH:mm:ss yyyy ZZZZZ").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, dd MMM yy HH:mm ZZZZZ").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss 'UT'").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, d MMM yyyy HH:mm:ss 'UT'").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("d MMM yyyy HH:mm:ss 'UT'").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss 'UT'").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, d MMM yyyy HH:mm:ss").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss ZZZZZ").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, MMM dd yyyy HH:mm:ss ZZZZZ").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, dd, MMM yyyy HH:mm:ss ZZZZZ").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE, d, MMM yyyy HH:mm:ss ZZZZZ").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("EEE MMM dd HH:mm:ss zzz yyyy").withLocale(Locale.ENGLISH));
-        sdfs.add(DateTimeFormatter.ofPattern("MMM, dd yyyy HH:mm:ss ZZZZZ"));
-        sdfs.add(DateTimeFormatter.ofPattern("MMM, d yyyy HH:mm:ss ZZZZZ"));
-        sdfs.add(DateTimeFormatter.ofPattern("MMM, dd yyyy HH:mm:ss a"));
-        sdfs.add(DateTimeFormatter.ofPattern("MMM, d yyyy HH:mm:ss a"));
-        patternsDate.add(DateTimeFormatter.ofPattern("EEE, dd MMM yyyy").withLocale(Locale.ENGLISH)); //only date
-        patternsDate.add(DateTimeFormatter.ofPattern("EEE, d MMM yyyy").withLocale(Locale.ENGLISH)); //only date
-        patternsDate.add(DateTimeFormatter.ofPattern("EEE, dd MMM yyyy zzz").withLocale(Locale.ENGLISH)); // only date
     }
 
     /**
      * Achives a instance of a Date Identifier
      *
-     * @return Te default date processor
+     * @return The default date processor
      */
     public static DateTimeIdentifier getDefault() {
         if (defaultDateTimeProcessor == null) {
@@ -122,9 +133,6 @@ public class DateTimeIdentifier {
 
         //Drop unnecesary spaces
         while (dateTimeStr.indexOf("  ") > 0) {
-            //if (debug) {
-            //    System.out.print("*");
-            //}
             dateTimeStr = dateTimeStr.replaceAll("  ", " ");
         }
 
@@ -136,56 +144,44 @@ public class DateTimeIdentifier {
         DateTimeFormatter dtf;
         
         // DateTime Using localized styles and default locale
-        for (Locale current:locales){
-            for(int i = 0; date==null && i < styles.length; i++){
-                for(int j = 0; date==null &&  j < styles.length; j++){                
-                    if (date == null){    
-                        dtf = DateTimeFormatter.ofLocalizedDateTime(styles[i], styles[j]).withLocale(current);
-                        // dtf = DateTimeFormatter.ofLocalizedDateTime(styles[i], styles[j]).withLocale(Locale.getDefault());
-                        try {
-                            date = LocalDateTime.parse(dateTimeStr , dtf);
-                            //System.out.println("Style " +styles[i] + ", " + styles[j] + ", " + current + ": " + date);
-                        } catch (DateTimeParseException pe) {
-                            //date = null;
-                        }
-                    }
+        for(int i = 0; date==null && i < styles.length; i++){
+            for(int j = 0; date==null &&  j < styles.length; j++){                
+                dtf = DateTimeFormatter.ofLocalizedDateTime(styles[i], styles[j]).withLocale(Locale.getDefault());
+                try {
+                    date = LocalDateTime.parse(dateTimeStr , dtf);
+                } catch (DateTimeParseException pe) {
+                    date = null;
                 }
-            } 
-            if (date!=null) break;
-        }
+            }
+        } 
+         
         
-        // DateTime Using localized styles
+        // DateTime using localized styles
          if (date == null) {
             for (Locale current:locales){
-                for(int i = 0; i < styles.length; i++){
-                    for(int j = 0; j < styles.length; j++){                
-                        if (date == null){    
-                            dtf = DateTimeFormatter.ofLocalizedDateTime(styles[i], styles[j]).withLocale(current);
-                            try {
-                                date = LocalDateTime.parse(dateTimeStr , dtf);
-                                //System.out.println("Style " +styles[i] + ", " + styles[j] + ", " + current + ": " + date);
-                            } catch (DateTimeParseException pe) {
-                                date = null;
-                            }
+                for(int i = 0; date == null && i < styles.length; i++){
+                    for(int j = 0; date == null && j < styles.length; j++){                
+                        dtf = DateTimeFormatter.ofLocalizedDateTime(styles[i], styles[j]).withLocale(current);
+                        try {
+                            date = LocalDateTime.parse(dateTimeStr , dtf);
+                        } catch (DateTimeParseException pe) {
+                            date = null;
                         }
-                    }
-                }  
+                   }
+                }
+                if (date != null) break;
             }
          }
         
         // DateTime using pattern 
-         if (date == null) {
+        if (date == null) {
             for(Locale current:locales){
-                //System.out.println("Locale:"+ current.toString());
                 byte i = 0;
-                while (date == null && i < sdfs.size()) {
-                    //System.out.println("format "+i);
+                while (date == null && i < sdfs.size()) {                    
                     try{
-                        date = LocalDateTime.parse(dateTimeStr, sdfs.get(i).withLocale(current));
-                        //System.out.println("Pattern DateTime");
-                        //System.out.println("caso -> "+ date);                    
+                        date = LocalDateTime.parse(dateTimeStr, sdfs.get(i).withLocale(current));                 
                     } catch (DateTimeParseException pe) {
-
+                        date = null;
                     }
                     i++;
                 }
@@ -196,39 +192,36 @@ public class DateTimeIdentifier {
         // Date Using localized styles
         if (date==null){
             for (Locale current:locales){
-                for(int i = 0; i < styles.length; i++){
-                    if (date == null){ 
-                        LocalDate ld;
-                        dtf = DateTimeFormatter.ofLocalizedDate(styles[i]).withLocale(current);
-                        try {
-                            ld = LocalDate.parse(dateTimeStr, dtf);
-                            //System.out.println("Style " + styles[i] + ", " + current + ": " + ld);
-                            date = ld.atStartOfDay();
-                        } catch (DateTimeParseException pe) {
-                            date = null;
-                        }
+                for(int i = 0; date == null && i < styles.length; i++){
+                    LocalDate ld;
+                    dtf = DateTimeFormatter.ofLocalizedDate(styles[i]).withLocale(current);
+                    try {
+                        ld = LocalDate.parse(dateTimeStr, dtf);
+                        date = ld.atStartOfDay();
+                    } catch (DateTimeParseException pe) {
+                        date = null;
                     }
-                }  
-            }
+                }
+                if (date!=null) break;
+            }  
         }
+        
         
         //Date using pattern 
         if (date == null) {            
             LocalDate ld;
             byte i = 0;
-            
-            //while (date == null && i < sdfs.size()) {
-            while (date == null && i < patternsDate.size()) {
-                try{
-                    ld = LocalDate.parse(dateTimeStr, patternsDate.get(i));
-                    //System.out.println("Pattern Date");
-                    //System.out.println("caso -> "+ ld);
-                    date = ld.atStartOfDay();
-                    
-                } catch (DateTimeParseException pe) {
-                    date=null;
+            for (Locale current:locales){
+                while (date == null && i < patternsDate.size()) {
+                    try{
+                        ld = LocalDate.parse(dateTimeStr, patternsDate.get(i).withLocale(current));
+                        date = ld.atStartOfDay();
+                    } catch (DateTimeParseException pe) {
+                        date=null;
+                    }
+                    i++;
                 }
-                i++;
+                if (date!=null) break;
             }
         }
          
