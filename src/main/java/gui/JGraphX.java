9ed4c8fb805207a9615967273f0227c31670e98e
Yeray Lage <ylagef@gmail.com>
2019-05-13 19:19:10 +0200

    So much GUI improvements.

--- src/main/java/gui/JGraphX.java
+++ src/main/java/gui/JGraphX.java
@@ -20,6 +20,7 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
@@ -29,12 +30,18 @@ public class JGraphX extends JFrame {
 
     private static final long serialVersionUID = -2764911804288120883L;
     private static final Logger logger = LogManager.getLogger(JGraphX.class);
-    private static int height = 1080;
+
     private final mxGraph graph;
     private Object parent;
-    private int pipeY = height / 2;
+
+    private int pipeLineY;
 
     private HashMap<String, PipeInfo> pipes;
+    private HashMap<String, String> generalConfiguration;
+
+    private String globalStyle = "fontSize=20;fontColor=black;verticalAlign=middle;editable=0;bendable=0;" +
+            "movable=0;resizable=0;foldable=0;deletable=0;rotable=0;cloneable=0;verticalLabelPosition=middle;" +
+            "labelPosition=center;rounded=1;";
 
     private boolean start = false;
     private boolean xml = false;
@@ -43,18 +50,21 @@ public class JGraphX extends JFrame {
         super("BDP4J");
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1920, height);
-        this.setVisible(true);
+
+        this.setMinimumSize(new Dimension(960, 540));
+        this.setMaximumSize(new Dimension(1920, 1080));
+        this.setPreferredSize(new Dimension(1920, 1080));
+        this.setLocationRelativeTo(null);
+
+        //Start on fullscreen
+        this.setExtendedState(this.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 
         this.pipes = pipes;
 
         graph = new mxGraph();
 
-        graph.setEdgeLabelsMovable(false);
-        graph.setCellsLocked(true);
-        graph.setCellsDisconnectable(false);
-
         parent = graph.getDefaultParent();
+        generalConfiguration = new HashMap<>();
     }
 
     public AbstractPipe start() {
@@ -63,18 +73,48 @@ public class JGraphX extends JFrame {
         SerialPipes serialPipes = new SerialPipes();
 
         graph.getModel().beginUpdate();
-
         try {
             // Start execution button
-            graph.insertVertex(parent, null, "Start", 10, 20, 80, 30);
+            graph.insertVertex(parent, null, "Start", 20, 50, 150, 60, globalStyle + "fontSize=30;fillColor=#81c784");
 
             // Generate xml button
-            graph.insertVertex(parent, null, "Generate xml", 10, 60, 80, 30);
+            graph.insertVertex(parent, null, "Generate xml", 200, 50, 270, 60, globalStyle + "fontSize=30;fillColor=#ff8a65;");
+
+            String fieldStyle = "editable=1;fillColor=#e3f2fd;textOpacity=70;";
+
+            Object options = graph.insertVertex(parent, null, null, 600, 150, 440, 280, globalStyle + "opacity=30;rounded=0;");
+
+            // Samples folder
+            Object samples1 = graph.insertVertex(options, null, "Samples folder", 20, 20, 150, 40, globalStyle);
+            Object samples2 = graph.insertVertex(options, null, "./samples", 220, 20, 200, 40, globalStyle + fieldStyle);
+            graph.insertEdge(options, null, null, samples1, samples2);
+
+            // Plugins folder
+            Object plugins1 = graph.insertVertex(options, null, "Plugins folder", 20, 70, 150, 40, globalStyle);
+            Object plugins2 = graph.insertVertex(options, null, "./plugins", 220, 70, 200, 40, globalStyle + fieldStyle);
+            graph.insertEdge(options, null, null, plugins1, plugins2);
+
+            // Output dir
+            Object outputDir1 = graph.insertVertex(options, null, "Output dir", 20, 120, 150, 40, globalStyle);
+            Object outputDir2 = graph.insertVertex(options, null, "./output", 220, 120, 200, 40, globalStyle + fieldStyle);
+            graph.insertEdge(options, null, null, outputDir1, outputDir2);
+
+            // Temp dir
+            Object tempDir1 = graph.insertVertex(options, null, "Temp dir", 20, 170, 150, 40, globalStyle);
+            Object tempDir2 = graph.insertVertex(options, null, "./temp", 220, 170, 200, 40, globalStyle + fieldStyle);
+            graph.insertEdge(options, null, null, tempDir1, tempDir2);
+
+            // Debug
+            Object debug1 = graph.insertVertex(options, null, "Debug", 20, 220, 150, 40, globalStyle);
+            Object debug2 = graph.insertVertex(options, null, "yes", 220, 220, 200, 40, globalStyle + fieldStyle);
+            graph.insertEdge(options, null, null, debug1, debug2);
 
             // Pipes buttons
             int y = 100;
-            for (String p : pipes.keySet()) graph.insertVertex(parent, null, p, 10, y += 50, 200, 30);
-
+            for (String p : pipes.keySet()) {
+                graph.insertVertex(parent, null, p, 20, y += 50, 450, 40, globalStyle + "fillColor=#80cbc4");
+            }
+            pipeLineY = y;
         } finally {
             graph.getModel().endUpdate();
         }
@@ -86,7 +126,7 @@ public class JGraphX extends JFrame {
                 new MouseAdapter() {
                     Object last = null;
                     Class lastType;
-                    private int lastPipeX = 150;
+                    private int lastPipeX = 20;
 
                     public void mouseClicked(MouseEvent e) {
                         Object cell = graphComponent.getCellAt(e.getX(), e.getY());
@@ -99,7 +139,7 @@ public class JGraphX extends JFrame {
                                 logger.info("[GUI] Generating XML...");
                                 xml = true;
 
-                            } else if (e.getX() < 205) {
+                            } else if (e.getX() < 460 && e.getY() < pipeLineY + 40) {
                                 AbstractPipe newPipe = null;
                                 boolean error = false;
 
@@ -135,7 +175,13 @@ public class JGraphX extends JFrame {
                                 if (!error) {
                                     serialPipes.add(newPipe);
 
-                                    Object added = graph.insertVertex(parent, null, graph.getLabel(cell), lastPipeX += 250, pipeY - 15, 200, 30);
+                                    if (lastPipeX + 450 >= 1900) {
+                                        pipeLineY += 100;
+                                        lastPipeX = 20;
+                                    }
+
+                                    Object added = graph.insertVertex(parent, null, graph.getLabel(cell), lastPipeX, pipeLineY + 100, 450, 40, globalStyle + "fillColor=#b0bec5");
+                                    lastPipeX += 550;
 
                                     if (last != null) {
                                         graph.insertEdge(parent, null, null, last, added);
@@ -152,6 +198,9 @@ public class JGraphX extends JFrame {
                 }
         );
 
+        this.pack();
+        this.setVisible(true);
+
         // Waiting until gui work is done for resume execution.
         while (!start && !xml) {
             try {
@@ -164,13 +213,25 @@ public class JGraphX extends JFrame {
         // Hide the gui
         this.setVisible(false);
 
-        // If xml generation option is selected via gui.
-        if (xml) generateXml(serialPipes);
+        // Save configurations
+        String[] configurations = {"samplesFolder", "pluginsFolder", "outputDir", "tempDir", "debugMode"};
+        int y = 120;
+        for (String key : configurations) {
+            Object cell = graphComponent.getCellAt(820, y += 50);
+            String value = graph.getLabel(cell);
+            generalConfiguration.put(key, value);
+        }
+
+        // XML generation from gui data
+        generateXml(serialPipes, generalConfiguration);
+
+        // Just generate XML and exit
+        if (xml) System.exit(0);
 
         return serialPipes;
     }
 
-    private void generateXml(SerialPipes pipesList) {
+    private void generateXml(SerialPipes pipesList, HashMap<String, String> generalConfiguration) {
         String xmlFilePath = "./generated/configuration.xml";
 
         try {
@@ -188,13 +249,13 @@ public class JGraphX extends JFrame {
 
             // General sub-elements
             Element samplesFolder = document.createElement("samplesFolder");
-            samplesFolder.appendChild(document.createTextNode("./samples"));
+            samplesFolder.appendChild(document.createTextNode(generalConfiguration.get("samplesFolder")));
             Element pluginsFolder = document.createElement("pluginsFolder");
-            pluginsFolder.appendChild(document.createTextNode("./plugins"));
+            pluginsFolder.appendChild(document.createTextNode(generalConfiguration.get("pluginsFolder")));
             Element outputDir = document.createElement("outputDir");
-            outputDir.appendChild(document.createTextNode("./output"));
+            outputDir.appendChild(document.createTextNode(generalConfiguration.get("outputDir")));
             Element tempDir = document.createElement("tempDir");
-            tempDir.appendChild(document.createTextNode("./temp"));
+            tempDir.appendChild(document.createTextNode(generalConfiguration.get("tempDir")));
 
             general.appendChild(samplesFolder);
             general.appendChild(pluginsFolder);
@@ -209,7 +270,7 @@ public class JGraphX extends JFrame {
             Attr resumable = document.createAttribute("resumable");
             resumable.setValue("yes");
             Attr debug = document.createAttribute("debug");
-            debug.setValue("yes");
+            debug.setValue(generalConfiguration.get("debugMode"));
             pipeline.setAttributeNode(resumable);
             pipeline.setAttributeNode(debug);
 
@@ -234,7 +295,6 @@ public class JGraphX extends JFrame {
             transformer.transform(domSource, streamResult);
 
             logger.info("[GUI] XML File successfully created.");
-            System.exit(0);
         } catch (ParserConfigurationException | TransformerException pce) {
             pce.printStackTrace();
         }
